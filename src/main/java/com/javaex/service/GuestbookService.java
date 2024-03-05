1ea5f9a5d7cd5c9941db531653f115be6501d0c8
hi <hi@DESKTOP-U9I16DL>
2024-03-05 18:30:18 +0900

    d

--- src/main/java/com/javaex/service/GuestbookService.java
+++ src/main/java/com/javaex/service/GuestbookService.java
@@ -4,20 +4,16 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.javaex.dao.GuestbookDao;
 import com.javaex.vo.GuestbookVo;
 
 @Service
-@RequestMapping(value="/guest")
 public class GuestbookService {
 
 	@Autowired
 	GuestbookDao guestbookDao;
 	
-	@RequestMapping(value="/add", method = {RequestMethod.GET, RequestMethod.POST})
 	public List<GuestbookVo> exeAddList() {
 		System.out.println("GuestbookService.exeAddList()");
 		
